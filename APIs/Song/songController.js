4c4d05f3a4d49cb6685466a27554b7276588cf78
ravikrpandey <ravikumarpandey.iet@gmail.com>
2025-08-15 11:26:29 +0530

    updated cofe of pupetter

--- APIs/Song/songController.js
+++ APIs/Song/songController.js
@@ -8,15 +8,14 @@ const youtubedl = require('yt-dlp-exec');
 const https = require('https');
 const path = require('path');
 const { exec } = require('child_process');
-const { where } = require("sequelize");
 const {saveFileAndGetNameByBase64} = require('../services/upload-files/service')
 const { Sequelize } = require('sequelize'); // Ensure Sequelize is imported
-// const cookiesPath = path.resolve(__dirname, '../../config/cookies.txt');
+const cookiesPath = path.resolve(__dirname, '../../config/cookies.txt');
 const streamPath = path.resolve(__dirname, '../python/stream_audio.py');
-const cookiesPath = '/home/ubuntu/sarmusicapi/cookies.txt'
+// const cookiesPath = '/home/ubuntu/sarmusicapi/cookies.txt'
 const fs = require('fs');
 const { spawn } = require("child_process");
-const { refreshCookies } = require("../../config/refresh-cookies");
+const { refreshCookies } = require("../../config/refresh-cookies.mjs");
 
 (async () => {
     console.log("🔄 Testing cookie refresh...");
